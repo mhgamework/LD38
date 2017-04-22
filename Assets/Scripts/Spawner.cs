c60c31a618b69792a6b06c313036ddadb44b5c27
Michiel Huygen <mhgamework@gmail;com>
2017-04-22 22:18:43 +0200

    Work on spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spawner : MonoBehaviour, ITimelineEntity
+    {
+        [SerializeField]
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(transform.position, 1);
+        }
+
+        public void OnDrawGizmosSelected()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/TimelineService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Collects all elements (trigger, or activatables) that can be scripted to implement level-specific behaviour
+    /// </summary>
+    public class TimelineService : Singleton<TimelineService>
+    {
+        private Dictionary<string, ITimelineEntity> dict = new Dictionary<string, ITimelineEntity>();
+
+        public void Register(ITimelineEntity entity)
+        {
+            dict[entity.Id] = entity;
+        }
+
+        public void UnRegister(ITimelineEntity entity)
+        {
+            dict.Remove(entity.Id);
+
+        }
+    }
+}
