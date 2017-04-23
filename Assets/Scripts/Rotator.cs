535331435589b111ca7add17ef1987341233fab2
Simon Chupts <simon.chuptys@gmail.com>
2017-04-23 17:14:34 +0200

    Added boulder spell prefab.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationDir = Vector3.zero;
+    [SerializeField]
+    [Range(0f, 1000f)]
+    private float speed = 100f;
+
+    void Update()
+    {
+        transform.localEulerAngles += rotationDir * speed * Time.deltaTime;
+    }
+}
