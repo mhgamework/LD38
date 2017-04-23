70950e058029c9316d150522a882b7c1bc00131c
Simon Chupts <simon.chuptys@gmail.com>
2017-04-23 20:35:59 +0200

    Gate added, levelbuyilding.

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject forceField = null;
+
+    public void OpenGate()
+    {
+        forceField.SetActive(false);
+    }
+
+    public void CloseGate()
+    {
+        forceField.SetActive(true);
+    }
+}
