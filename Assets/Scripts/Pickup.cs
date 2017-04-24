2548136e30f62d0b10b0dd3ee3301c003cdaa57a
Simon Chupts <simon.chuptys@gmail.com>
2017-04-24 16:52:52 +0200

    Pickups implemented. Added PlanetPlacer.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource = null;
+    private bool isPickedUp;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isPickedUp)
+            return;
+        isPickedUp = true;
+
+        Debug.Log("You earned 5 points!!");
+        StartCoroutine(OnPickup());
+    }
+
+    IEnumerator OnPickup()
+    {
+        audioSource.enabled = true;
+        GetComponent<Renderer>().enabled = false;
+
+        yield return new WaitForSeconds(1f);
+
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/PlanetPlacer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+public class PlanetPlacer : MonoBehaviour
+{
+    public float CircleRadius = 100f;
+
+    public void PlaceObject(Ray ray)
+    {
+        var o = new GameObject("Sphere");
+        var sphere = o.AddComponent<SphereCollider>();
+        sphere.radius = CircleRadius;
+
+        RaycastHit hit;
+        if (!sphere.Raycast(ray, out hit, 1000))
+        {
+            DestroyImmediate(o);
+            return;
+        }
+
+        var dir = hit.point.normalized;
+        transform.up = dir;
+        transform.position = hit.point;
+
+        DestroyImmediate(o);
+    }
+}
+#endif
